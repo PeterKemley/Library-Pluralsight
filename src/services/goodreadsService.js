3fd39ff146596f4e2f0afa8061048076b4785482
PeterKemley <peter.kemley@accenture.com>
2020-01-21 10:48:54 +0000

    Changes to Signup!

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,10 +1,29 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ desvription: 'our description' });
+      axios.get('https://www.goodreads.com/book/show/656.xml?key=JVSWSWYEAhuZTx6TT2AYw')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
   return { getBookById };
 }
-
 module.exports = goodreadsService;
